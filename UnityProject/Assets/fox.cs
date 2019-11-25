89023d83b845bd0a131625c49a9bf0562dc7d2a0
you92524 <57396776+you92524@users.noreply.github.com>
2019-11-26 01:53:41 +0800

    未完成

---
+++ UnityProject/Assets/fox.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fox : MonoBehaviour
+{
+    [Header("移動速度"), Range(1f, 100f), Tooltip("控制小雞移動速度")]
+    public float speed = 10f;
+    
+    public Rigidbody2D Rig ;
+    
+    public bool sr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float speed = Input.GetAxisRaw("Horizontal") * Time.deltaTime;
+        transform.Rotate(0, speed, 0);
+    }
+    
+}
