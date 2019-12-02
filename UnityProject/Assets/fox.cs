f6deacfcedd293882821609d8dd89f4d75cd5068
you92524 <57396776+you92524@users.noreply.github.com>
2019-12-02 21:30:57 +0800

    作業完成

--- UnityProject/Assets/fox.cs
+++ UnityProject/Assets/fox.cs
@@ -1,14 +1,42 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class fox : MonoBehaviour
 {
-    [Header("移動速度"), Range(1f, 100f), Tooltip("控制小雞移動速度")]
-    public float speed = 10f;
+    public float speed = 10f,y=1f;
+    public Rigidbody2D Rig;
+    public SpriteRenderer Sr;
     
-    public Rigidbody2D Rig ;
-    
-    public bool sr;
+    //移動功能
+    private void Move()
+    {
+        Vector2 pos = new Vector2( speed*Input.GetAxisRaw("Horizontal") * Time.deltaTime*10000f, y);
+
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                print("D");
+           
+
+                Rig.AddForce(pos, ForceMode2D.Force);
+
+                //狐狸翻面
+                Sr.flipX = false;
+
+             
+            
+            }
+            else if(Input.GetKeyDown(KeyCode.A))
+            {
+                print("A");
+            
+
+                Rig.AddForce(pos, ForceMode2D.Force);
+
+                //狐狸翻面
+                Sr.flipX = true;
+
+             
+            }
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -16,10 +44,9 @@ public class fox : MonoBehaviour
         
     }
 
+    // Update is called once per frame
     void Update()
     {
-        float speed = Input.GetAxisRaw("Horizontal") * Time.deltaTime;
-        transform.Rotate(0, speed, 0);
+        Move();
     }
-    
 }
